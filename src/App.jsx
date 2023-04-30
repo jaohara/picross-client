deb7ffacfe71511e86d99fb3619e4d91820fc78a
John O'Hara <johnoharaa@gmail.com>
2023-04-30 08:15:43 -0700

    Testing Firebase deployment on push to main

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hey! I&apos;ve been edited locally and pushed to <pre>main</pre> to see if the Firebase 
+          Hosting Deployment action was properly set up.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
