f6a2e3ea6ad37dc7fb56bdbd92f7ca81f0163757
John O'Hara <johnoharaa@gmail.com>
2023-04-29 19:53:14 -0700

    Added hashPuzzleGrid function, added basic api functions, initialized firebase hosting

---
+++ src/firebase/api.js
@@ -0,0 +1,105 @@
+import {
+  collection,
+  getDoc,
+  getDocs,
+  orderBy,
+  query,
+  setDoc,
+  Timestamp,
+  updateDoc,
+  where,
+  writeBatch,
+} from "firebase/firestore";
+
+import { db } from "./firebase";
+
+const usersCollectionRef = collection(db, "users");
+const puzzlesCollectionRef = collection(db, "puzzles");
+
+// user-related api functions
+export async function createUserEntity(newUser, id) {
+  console.log("createUserEntity: received newUser: ", newUser);
+  // assuming the user is valid, as the registration process would have caught an error
+
+  // TODO: uncomment after you know puzzles are created with the proper timestamp
+  const userCreatedTimestamp = Timestamp.now();
+  newUser["createdTimestamp"] = userCreatedTimestamp;
+  newUser["updatedTimestamp"] = userCreatedTimestamp;
+
+  try {
+    const docRef = doc(usersCollectionRef, id);
+    const result = await setDoc(docRef, newUser);
+    return result;
+  }
+  catch (error) {
+    return error;
+  }
+}
+
+// puzzle-related api functions
+
+// TODO: Probably adapt this into a "getAllOfficialPuzzles" function
+export async function getUserPuzzles(
+  authorId, 
+  setUserPuzzles,
+  orderByField = "updatedTimestamp",
+  // orderByField = "createdTimestamp",
+){
+  console.log("api: getUserPuzzles: received authorId:", authorId);
+
+  // build query to get all puzzles for the given authorId
+  const userPuzzlesQuery = query(
+    puzzlesCollectionRef, 
+    // TODO: Modify this for all official puzzles
+    where("authorId", "==", authorId),
+    orderBy(orderByField, "desc"),
+  );
+  
+  // execute the query
+  const userPuzzlesSnapshot = await getDocs(userPuzzlesQuery);
+
+  // get the documents from the snapshot
+  const userPuzzles = userPuzzlesSnapshot.docs.map((puzzleDocRef) => {
+    const puzzleDocData = puzzleDocRef.data();
+    puzzleDocData.id = puzzleDocRef.id;
+    return puzzleDocData;
+  });
+
+  console.log("api: getUserPuzzles: received the following user puzzles: ", userPuzzles);
+
+  // store the puzzles 
+  setUserPuzzles(userPuzzles);
+}
+
+
+export async function getPuzzleGridForPuzzle(puzzleData) {
+  console.log("api: getPuzzleGridForPuzzle: received puzzleData: ", puzzleData);
+
+  // pull out puzzleId
+  const puzzleId = puzzleData.id;
+
+  if (!puzzleId) {
+    console.error("api: getPuzzleGridForPuzzle: puzzleData has no id attached to it.");
+    return;
+  }
+
+  let loadedGrid = [];
+
+  const puzzleDocRef = doc(puzzlesCollectionRef, puzzleData.id);
+  const gridCollectionRef = collection(puzzleDocRef, "grid");
+  const gridDocRef = doc(gridCollectionRef, gridSubcollectionDocId);
+
+  // const gridSnapshot = await gridDocRef.get();
+  const gridSnapshot = await getDoc(gridDocRef);
+
+  if (gridSnapshot.exists()) {
+    const gridData = gridSnapshot.data();
+
+    if (gridData.gridString) {
+      const parsedGrid = JSON.parse(gridData.gridString);
+      loadedGrid = parsedGrid;
+    }
+  }
+
+  return loadedGrid;
+}
