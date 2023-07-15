77e472a36c142da2d412c603fec3fbbb5377c1ea
John O'Hara <johnoharaa@gmail.com>
2023-07-15 09:28:48 -0700

    Added updateGameRecord and setTopLevelGameRecord, began fixing countGameRecords

--- functions/index.js
+++ functions/index.js
@@ -19,19 +19,8 @@ const {
 const { Firestore } = require("@google-cloud/firestore");
 const db = new Firestore();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
-
-
 // Test function to return a count of all game records in the document database
 exports.countGameRecords = onRequest(async (req, res) => {
-  logger.info("Invoking countGameRecords...");
-
   // temp variable - remove with check to permanent storage
   const cacheIsValid = false;
   // if cache is valid - serve info from cache
@@ -42,45 +31,17 @@ exports.countGameRecords = onRequest(async (req, res) => {
   
   // if cache is invalid (over ~6hrs since last update)
   try {
-    const gameRecords = [];
+    // const gameRecords = [];
+    const gameRecords = "test as a string";
+    const gameRecordsSnapshot = await db.collection("gameRecords").get();
 
-    const batch = db.batch();
-    const usersRef = db.collection('users');
-    const usersSnapshot = await usersRef.get();
-
-    
-    /*
-      TODO: logic here is kind of broken, and might need to be reconsidered with the 
-      top-level /gameRecords collection.
-
-      Querying all of the documents in the user subcollections is going to cause a LOT of 
-      read operations. It's not really feasible to run this multiple times a day - I think we 
-      need to gun for once or twice a day, max. 
-
-      Anyway, using this top-level collection, we won't need to query the user records at all, 
-      which will cut out some reads. We'll also have simpler (non-batched) read logic for getting
-      everything from /gameRecords.
-
-      Maybe return to work here?
-          
-    */
-    usersSnapshot.forEach((userDoc) => {
-      // get a reference to the user's "gameRecords" collection
-      const gameRecordsRef = userDoc.ref.collection('gameRecords');
-
-      // create a read op for the collection
-      const gameRecordsQuery = gameRecordsRef.get();
-      batch.set(gameRecordsQuery);
-    });
-
-    // execute the batched read operation
-    const results = await batch.commit();
+    logger.info("countGameRecords: got gameRecordsSnapshot:", gameRecordsSnapshot);
 
     // iterate over the batched results and add to the collection
-    results.forEach((gameRecordsSnapshot) => 
-      gameRecordsSnapshot.forEach((gameRecordDoc) => 
-        gameRecords.push(gameRecordDoc.data()))
-    );
+    // results.forEach((gameRecordsSnapshot) => 
+    //   gameRecordsSnapshot.forEach((gameRecordDoc) => 
+    //     gameRecords.push(gameRecordDoc.data()))
+    // );
 
     // send the gameRecords as a response
     res.status(200).json(gameRecords);
@@ -94,19 +55,14 @@ exports.countGameRecords = onRequest(async (req, res) => {
 // TODO: WORK ON THIS MORE
 // function to create a dupe, top-level gameRecord on creation of a completed user gameRecord
 exports.createGameRecord = 
-  onDocumentCreated('users/{userId}/gameRecords/{gameRecordId}', async (event, context) => {
-  // functions.firestore
-  // .document('users/{userId}/gameRecords/{gameRecordId}')
-  // .onCreate(async (snapshot, context) => {
+  onDocumentCreated('users/{userId}/gameRecords/{gameRecordId}', async (event) => {
     try {
-      const { gameRecordId } = context.params;
-      
-      // const gameRecordData = snapshot.data();
+      const { gameRecordId } = event.params;
       const gameRecordData = event.data.data();
 
       // bail out if record isn't complete
       if (!gameRecordData.completed) { 
-        logger.info("GameRecord isn't complete, not duplicating.")
+        logger.info("createGameRecord: GameRecord isn't complete, not duplicating.")
         return;
       }
 
@@ -114,14 +70,51 @@ exports.createGameRecord =
       delete gameRecordData.userId;
 
       // create a dupe entry in the top-level /gameRecords coll
-      const gameRecordsRef = db.collection('gameRecords');
-      await gameRecordsRef.doc(gameRecordId).set(gameRecordData);
+      // const gameRecordsRef = db.collection('gameRecords');
+      // await gameRecordsRef.doc(gameRecordId).set(gameRecordData);
+      // logger.info(`createGameRecord: successfully created /gameRecord/${gameRecordId}:`, gameRecordData);
+
     }
     catch (error) {
-      logger.error("Error duplicating gameRecord:", error);
+      logger.error("createGameRecord: Error duplicating gameRecord:", error);
     }
   });
 
 // exports.updateGameRecord = functions.firestore
 //   .document('users/{userId}/gameRecords/{gameRecordId}')
 //   .on
+
+exports.updateGameRecord = 
+  onDocumentUpdated('users/{userId}/gameRecords/{gameRecordId}', async (event) => {
+    try {
+      const { gameRecordId } = event.params;
+      
+      const oldGameRecordData = event.data.before.data();
+      const newGameRecordData = event.data.after.data();
+
+      const hasBeenCompleted = !oldGameRecordData.completed && newGameRecordData.completed
+
+      if (!hasBeenCompleted) { 
+        logger.info("updateGameRecord: GameRecord isn't complete, not duplicating.")
+        return;
+      }
+
+      // TODO: filter unnecessary fields here
+      delete newGameRecordData.userId;
+      await setTopLevelGameRecord(gameRecordId, newGameRecordData);
+
+      // create a dupe entry in the top-level /gameRecords coll
+      // const gameRecordsRef = db.collection('gameRecords');
+      // await gameRecordsRef.doc(gameRecordId).set(newGameRecordData);
+      // logger.info(`createGameRecord: successfully created /gameRecord/${gameRecordId}:`, gameRecordData);
+    }
+    catch (error) {
+      logger.error("createGameRecord: Error duplicating gameRecord:", error);
+    }
+  });
+
+async function setTopLevelGameRecord(gameRecordId, gameRecordData) {
+  const gameRecordsRef = db.collection('gameRecords');
+  await gameRecordsRef.doc(gameRecordId).set(gameRecordData);
+  logger.info(`setTopLevelGameRecord: successfully created /gameRecord/${gameRecordId}:`, gameRecordData);
+}
