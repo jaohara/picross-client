9f8e58e12fc3ddba43d132cf7b4c057a70b29b18
John O'Hara <johnoharaa@gmail.com>
2023-10-17 12:41:33 -0700

    Cleaned up the DailyStreakVisualization

--- src/components/DailyStreakVisualization/DailyStreakVisualization.jsx
+++ src/components/DailyStreakVisualization/DailyStreakVisualization.jsx
@@ -8,154 +8,22 @@ import "./DailyStreakVisualization.scss";
 
 import { WEEKDAY_STRINGS } from '../../constants';
 
-import convertFromFirestoreTimestampToDate from '../../utils/convertFromFirestoreTimeStampToDate';
+// import convertFromFirestoreTimestampToDate from '../../utils/convertFromFirestoreTimeStampToDate';
 import findKeyWithLargestArray from '../../utils/findKeyWithLargestArray';
 
-const weekdays = [" ", ...WEEKDAY_STRINGS];
+// removing this space, not sure what the week number header adds to the table
+// const weekdays = [" ", ...WEEKDAY_STRINGS];
+const weekdays = [...WEEKDAY_STRINGS];
 
-// // TODO: REMOVE, TEMP DATA
-// const TEMP_GAME_RECORDS = [
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1688462821, nanoseconds: 0 },
-//     gameTimer: 214500,
-//     testGameRecord: true,
-//     moves: 55,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1688384400, nanoseconds: 0 },
-//     gameTimer: 289000,
-//     testGameRecord: true,
-//     moves: 76,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1688380800, nanoseconds: 0 },
-//     gameTimer: 191000,
-//     testGameRecord: true,
-//     moves: 42,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1688377200, nanoseconds: 0 },
-//     gameTimer: 339000,
-//     testGameRecord: true,
-//     moves: 68,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1688313600, nanoseconds: 0 },
-//     gameTimer: 150000,
-//     testGameRecord: true,
-//     moves: 33,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1688197200, nanoseconds: 0 },
-//     gameTimer: 346000,
-//     testGameRecord: true,
-//     moves: 80,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1688190000, nanoseconds: 0 },
-//     gameTimer: 173000,
-//     testGameRecord: true,
-//     moves: 58,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1687981200, nanoseconds: 0 },
-//     gameTimer: 212000,
-//     testGameRecord: true,
-//     moves: 49,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1687472400, nanoseconds: 0 },
-//     gameTimer: 243000,
-//     testGameRecord: true,
-//     moves: 63,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1687081200, nanoseconds: 0 },
-//     gameTimer: 116000,
-//     testGameRecord: true,
-//     moves: 39,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1686882000, nanoseconds: 0 },
-//     gameTimer: 304000,
-//     testGameRecord: true,
-//     moves: 72,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1686878400, nanoseconds: 0 },
-//     gameTimer: 233000,
-//     testGameRecord: true,
-//     moves: 61,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1686874800, nanoseconds: 0 },
-//     gameTimer: 398000,
-//     testGameRecord: true,
-//     moves: 82,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1686871200, nanoseconds: 0 },
-//     gameTimer: 221000,
-//     testGameRecord: true,
-//     moves: 59,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1686788400, nanoseconds: 0 },
-//     gameTimer: 278000,
-//     testGameRecord: true,
-//     moves: 77,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1686692400, nanoseconds: 0 },
-//     gameTimer: 186000,
-//     testGameRecord: true,
-//     moves: 40,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1686596400, nanoseconds: 0 },
-//     gameTimer: 317000,
-//     testGameRecord: true,
-//     moves: 70,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1686500400, nanoseconds: 0 },
-//     gameTimer: 222000,
-//     testGameRecord: true,
-//     moves: 56,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1686010800, nanoseconds: 0 },
-//     gameTimer: 130000,
-//     testGameRecord: true,
-//     moves: 31,
-//   },
-//   {
-//     completed: true,
-//     lastPlayed: { seconds: 1684234800, nanoseconds: 0 },
-//     gameTimer: 139800,
-//     testGameRecord: true,
-//     moves: 38,
-//   },
-// ];
+/*
+  To add the week numbers back to the table:
+  
+    1. uncomment the above line that adds the single-space string to the weekdays array
+    2. uncomment the lines in getPastDates that push the week number to the pastDates array
+    3. uncomment the ternary in the render function where you map the first item in the week
+       array to a special table header cell  
+*/
+
 
 // // TODO: Also temp, but I might reuse this name later on
 // //  this should also be a prop
@@ -164,12 +32,6 @@ const weekdays = [" ", ...WEEKDAY_STRINGS];
 //   return record;
 // });
 
-// TODO: Move this logic - it should be within the body of the component 
-// BEGIN LOGIC
-// const gameRecordsByDay = {};
-
-
-// END OF LOGIC TO MOVE INTO COMPONENT
 
 const DailyStreakVisualization = ({
   gameRecords,
@@ -194,19 +56,21 @@ const DailyStreakVisualization = ({
     "daily-streak-cell-100",
   ];
 
-  const getRandomClassName = () => 
-    dailyStreakCellNames[Math.floor(Math.random() * dailyStreakCellNames.length)];
+  // TODO: Remove after finished with DailyStreakVisualization
+  // used to test cell colors by generating a random dailyStreakCellNames className
+  // const getRandomClassName = () => 
+  //   dailyStreakCellNames[Math.floor(Math.random() * dailyStreakCellNames.length)];
 
   const getCellClassName = (cellDate) => {
     if (!cellDate || !dailyMax) {
       return;
     }
 
-    console.log(`DSV: getCellClassName: For ${cellDate}, we have ${getPuzzleCountOnDay(cellDate)}`);
+    console.log(`DSVisualization: getCellClassName: For ${cellDate}, we have ${getPuzzleCountOnDay(cellDate)}`);
     let classNameIndex = Math.floor((getPuzzleCountOnDay(cellDate) / dailyMax) * 10);
     const cellClassName = dailyStreakCellNames[classNameIndex];
-    console.log(`DSV: getCellClassName: classNameIndex is ${classNameIndex}`);
-    console.log(`DSV: getCellClassName: className is ${cellClassName}`);
+    // console.log(`DSVisualization: getCellClassName: classNameIndex is ${classNameIndex}`);
+    // console.log(`DSVisualization: getCellClassName: className is ${cellClassName}`);
     
     return cellClassName;
   };
@@ -228,8 +92,8 @@ const DailyStreakVisualization = ({
     initialDayDelta = 77
   ) => {
     const currentDate = new Date();
-    console.log("DailyStreakVisualization: getPastDates: currentDate is:", currentDate);
-    console.log("DailyStreakVisualization: getPastDates: currentDate.getDay():", currentDate.getDay());
+    // console.log("DailyStreakVisualization: getPastDates: currentDate is:", currentDate);
+    // console.log("DailyStreakVisualization: getPastDates: currentDate.getDay():", currentDate.getDay());
     const pastDates = [];
 
     // get actual day delta
@@ -248,7 +112,9 @@ const DailyStreakVisualization = ({
       
       if (currentWeekday === 0) {
         pastDates.push([]);
-        pastDates[currentWeek].push(currentWeek);
+        // this line adds the week number, in tandem with a one-space string prepended to 
+        //  the weekdays array
+        // pastDates[currentWeek].push(currentWeek);
       }
 
       const millisOffset = i * 24 * 60 * 60 * 1000;
@@ -274,7 +140,7 @@ const DailyStreakVisualization = ({
       return;
     }
 
-    console.log("DSV: useEffect: Received gameRecords as prop:", gameRecords);
+    console.log("DSVisualization: useEffect: Received gameRecords as prop:", gameRecords);
 
     const newGameRecordsByDay = {};
 
@@ -303,10 +169,10 @@ const DailyStreakVisualization = ({
     largestKey.current = findKeyWithLargestArray(newGameRecordsByDay);
     const newDailyMax = newGameRecordsByDay[largestKey.current].length;
     
-    console.log("DSV: useEffect: gameRecords:", gameRecords);
-    console.log("DSV: useEffect: newGameRecordsByDay:", newGameRecordsByDay);
-    console.log("DSV: useEffect: largestKey:", largestKey.current);
-    console.log("DSV: useEffect: newDailyMax:", newDailyMax);
+    console.log("DSVisualization: useEffect: gameRecords:", gameRecords);
+    console.log("DSVisualization: useEffect: newGameRecordsByDay:", newGameRecordsByDay);
+    console.log("DSVisualization: useEffect: largestKey:", largestKey.current);
+    console.log("DSVisualization: useEffect: newDailyMax:", newDailyMax);
 
     setDailyMax(newDailyMax);
     setGameRecordsByDay(newGameRecordsByDay);
@@ -338,9 +204,10 @@ const DailyStreakVisualization = ({
                 >
                   {
                     week.map((cellDate, index) => (
-                      index === 0 ? (
-                        <div className="daily-streak-cell-info" key={index}>{cellDate + 1}</div>
-                      ) : (
+                      // this commented code adds the week numbers back
+                      // index === 0 ? (
+                      //   <div className="daily-streak-cell-info" key={index}>{cellDate + 1}</div>
+                      // ) : (
                         <div 
                           className={`daily-streak-cell ${getCellClassName(cellDate)}`} 
                           key={index}
@@ -354,7 +221,7 @@ const DailyStreakVisualization = ({
                           </div>
                             
                         </div> 
-                      )
+                      // )
                     ))
                   }
                 </div>
